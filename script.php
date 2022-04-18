5901170fffc247c5104ef696593c565db0ce7304
pmleconte <pascal.leconte@conseilgouz.com>
2022-04-18 18:14:52 +0200

    Add files via upload

---
+++ script.php
@@ -0,0 +1,167 @@
+<?php
+/**
+* Simple Accordeon Plugin  - Joomla 4.0.0 Module 
+* Version			: 2.0.2
+* Package			: Simple Accordeon Plugin
+* copyright 		: Copyright (C) 2022 ConseilGouz. All rights reserved.
+* license    		: http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+*/
+// No direct access to this file
+defined('_JEXEC') or die;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Filesystem\Folder;
+use Joomla\CMS\Version;
+use Joomla\CMS\Filesystem\File;
+
+class plgcontentsimpleaccordeonInstallerScript
+{
+	private $min_joomla_version      = '4.0.0';
+	private $min_php_version         = '7.2';
+	private $name                    = 'Plugin Simple Accordeon';
+	private $exttype                 = 'plugin';
+	private $extname                 = 'simpleaccordeon';
+	private $previous_version        = '';
+	private $dir           = null;
+	private $installerName = 'plgcontentsimpleaccordeoninstaller';
+	public function __construct()
+	{
+		$this->dir = __DIR__;
+		$this->lang = Factory::getLanguage();
+		$this->lang->load($this->extname);
+	}
+
+    function preflight($type, $parent)
+    {
+		if ( ! $this->passMinimumJoomlaVersion())
+		{
+			$this->uninstallInstaller();
+			return false;
+		}
+
+		if ( ! $this->passMinimumPHPVersion())
+		{
+			$this->uninstallInstaller();
+			return false;
+		}
+		// To prevent installer from running twice if installing multiple extensions
+		if ( ! file_exists($this->dir . '/' . $this->installerName . '.xml'))
+		{
+			return true;
+		}
+    }
+    
+    function postflight($type, $parent)
+    {
+		if (($type=='install') || ($type == 'update')) { // remove obsolete dir/files
+			$this->postinstall_cleanup();
+		}
+
+		switch ($type) {
+            case 'install': $message = Text::_('ISO_POSTFLIGHT_INSTALLED'); break;
+            case 'uninstall': $message = Text::_('ISO_POSTFLIGHT_UNINSTALLED'); break;
+            case 'update': $message = Text::_('ISO_POSTFLIGHT_UPDATED'); break;
+            case 'discover_install': $message = Text::_('ISO_POSTFLIGHT_DISC_INSTALLED'); break;
+        }
+		return true;
+    }
+	private function postinstall_cleanup() {
+		$obsloteFiles = [sprintf("%s/plugins/content/%s/simpleaccordeon.css", JPATH_SITE, $this->extname),
+						 sprintf("%s/plugins/system/%s/simpleaccordeon.js", JPATH_SITE, $this->extname)];
+		foreach ($obsloteFiles as $file)
+		{
+			if (@is_file($file))
+			{
+				File::delete($file);
+			}
+		}
+		$j = new Version();
+		$version=$j->getShortVersion(); 
+		$version_arr = explode('.',$version);
+		if (($version_arr[0] == "4") || (($version_arr[0] == "3") && ($version_arr[1] == "10"))) {
+			// Delete 3.9 and older language files
+			$langFiles = [
+				sprintf("%s/language/en-GB/en-GB.plg_content_%s.ini", JPATH_ADMINISTRATOR, $this->extname),
+				sprintf("%s/language/en-GB/en-GB.plg_content_%s.sys.ini", JPATH_ADMINISTRATOR, $this->extname),
+				sprintf("%s/language/fr-FR/fr-FR.plg_content_%s.ini", JPATH_ADMINISTRATOR, $this->extname),
+				sprintf("%s/language/fr-FR/fr-FR.plg_content_%s.sys.ini", JPATH_ADMINISTRATOR, $this->extname),
+			];
+			foreach ($langFiles as $file) {
+				if (@is_file($file)) {
+					File::delete($file);
+				}
+			}
+		}
+		$db = Factory::getDbo();
+        $conditions = array(
+            $db->qn('type') . ' = ' . $db->q('plugin'),
+            $db->qn('element') . ' = ' . $db->quote('simpleaccordeon')
+        );
+        $fields = array($db->qn('enabled') . ' = 1');
+
+        $query = $db->getQuery(true);
+		$query->update($db->quoteName('#__extensions'))->set($fields)->where($conditions);
+		$db->setQuery($query);
+        try {
+	        $db->execute();
+        }
+        catch (RuntimeException $e) {
+            JLog::add('unable to enable plugin simpleaccordeon', JLog::ERROR, 'jerror');
+        }
+
+	}
+
+	// Check if Joomla version passes minimum requirement
+	private function passMinimumJoomlaVersion()
+	{
+		$j = new Version();
+		$version=$j->getShortVersion(); 
+		if (version_compare($version, $this->min_joomla_version, '<'))
+		{
+			Factory::getApplication()->enqueueMessage(
+				'Incompatible Joomla version : found <strong>' . $version . '</strong>, Minimum : <strong>' . $this->min_joomla_version . '</strong>',
+				'error'
+			);
+
+			return false;
+		}
+
+		return true;
+	}
+
+	// Check if PHP version passes minimum requirement
+	private function passMinimumPHPVersion()
+	{
+
+		if (version_compare(PHP_VERSION, $this->min_php_version, '<'))
+		{
+			Factory::getApplication()->enqueueMessage(
+					'Incompatible PHP version : found  <strong>' . PHP_VERSION . '</strong>, Minimum <strong>' . $this->min_php_version . '</strong>',
+				'error'
+			);
+			return false;
+		}
+
+		return true;
+	}
+	private function uninstallInstaller()
+	{
+		if ( ! JFolder::exists(JPATH_PLUGINS . '/system/' . $this->installerName)) {
+			return;
+		}
+		$this->delete([
+			JPATH_PLUGINS . '/system/' . $this->installerName . '/language',
+			JPATH_PLUGINS . '/system/' . $this->installerName,
+		]);
+		$db = Factory::getDbo();
+		$query = $db->getQuery(true)
+			->delete('#__extensions')
+			->where($db->quoteName('element') . ' = ' . $db->quote($this->installerName))
+			->where($db->quoteName('folder') . ' = ' . $db->quote('system'))
+			->where($db->quoteName('type') . ' = ' . $db->quote('plugin'));
+		$db->setQuery($query);
+		$db->execute();
+		Factory::getCache()->clean('_system');
+	}
+	
+}
---
+++ simpleaccordeon.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * @version		2.0.2
+ * @package		Simple Accordeon content plugin
+ * @author		ConseilGouz
+ * @copyright	Copyright (C) 2022 ConseilGouz. All rights reserved.
+ * @license		GNU/GPL v2; see LICENSE.php
+ **/
+// no direct access
+defined( '_JEXEC' ) or die;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\HTML\HTMLHelper;
+
+class plgContentSimpleAccordeon extends CMSPlugin {
+	public function onContentPrepare( $context, &$article, &$params, $page = 0 )
+	{
+		// first figure out whether to use article id or item_id:
+		if (!((isset($article->item_id)) || (isset($article->id)))) {return;} 
+		$id = isset($article->item_id) ? $article->item_id : $article->id;
+		// simple performance check to determine whether bot should process further
+		$shortcode = $this->params->get('shortcode','accordeon'); 
+		if (strpos($article->text, '{'.$shortcode.'') === false ) {
+			return true;
+		}
+		$regex = '/{'.$shortcode.'[\s\S]+?{\/'.$shortcode.'}*\s*/'; // get each accordeon chunk
+	 	// find all instances of slides and put in $matches
+		if (preg_match_all($regex, $article->text, $accordeon)) { // make sure there is a match
+			$plg	= 'media/simpleaccordeon/';
+			/** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
+			$wa = Factory::getApplication()->getDocument()->getWebAssetManager();
+			$wa->registerAndUseStyle('simpleaccordeon', $plg.'/css/simpleaccordeon.css');
+			$bgcolor = $this->params->get('backgroundcolor','#eeeeee'); 
+			$chrcolor = $this->params->get('chrcolor','#000000'); 
+			$selbgcolor = $this->params->get('selbgcolor','#c7c7bf'); 
+			$fontsize = $this->params->get('fontsize','15px'); 
+			$wa->addInLineStyle('button.accordeon {background-color: '.$bgcolor.';color:'.$chrcolor.';font-size:'.$fontsize.';line-height:'.$fontsize.'}');
+			$wa->addInLineStyle('button.accordeon:not(.collapsed), button.accordeon:hover {background-color: '.$selbgcolor.' !important;}');
+			// prep more specific regex:
+			// add something to strip <br>s, like (?:<br ?\/?\>)
+			$regex = '/(?:<(div|p)[^>]*>)?{'.$shortcode.'(?:=(.+))?}(?(1) *<\/\1>)?([\s\S]+?)(?:<(div|p)[^>]*>)?{\/'.$shortcode.'}(?(4) *<\/\4>)/i';
+			foreach($accordeon[0] as $key=>$accordeon) {
+				if (preg_match_all($regex, $accordeon, $matches, PREG_SET_ORDER)) { // ensure the more specific regex matches
+					$identifier = 'acc' . $id . '_' . ($key+1);
+					foreach ($matches as $match) {
+						// $match[0] is full pattern match, $match[1] is an html tag wrapping '{accordeon ...}' (if found),
+						// $match[2] is the title (toggler), $match[3] is the actual block,
+						// $match[4] is an html tag wrapping '{/accordeon}' (if found)
+						$output = '';
+						// Make sure we've got what we need:
+						if ($match[3]) {
+							$tag = array();
+							//  |open to open accordion as default behavior
+							$strcollapsed = 'collapsed';
+							$straria="false";
+							$show = "";
+							if (strpos($match[2],'|open') > 0 ) {
+							    $match[2] = str_replace('|open','',$match[2]);
+								$strcollapsed = '';
+								$straria="true";
+								$show = "show";
+							}
+							HTMLHelper::_('bootstrap.collapse', '#' . $identifier);
+							$tag[1]= '<button id="'.$identifier.'" data-bs-toggle="collapse" data-bs-target="#panel'.$identifier.'" aria-expanded="'.$straria.'" aria-controls="panel'.$identifier.'" class="accordeon '.$strcollapsed.'">'.$match[2].'</button>';
+							$tag[2] = '<div class="collapse '.$show.' " id="panel'.$identifier.'">'.$match[3].'</div>';
+							$output .= $tag[1] . $tag[2];
+						}
+						$article->text = str_replace($match[0], $output, $article->text);
+					}
+				}
+			}
+			$article->text = $article->text;
+		}
+	}
+}
+?>
